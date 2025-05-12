2bb01e631e142a69dfd34309b283ec46496f3c30
Thiago Mamede <thiago.mamede@outlook.com.br>
2025-05-12 18:43:53 -0300

    final commit

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -7,29 +7,39 @@ const CartItem = ({ onContinueShopping }) => {
   const cart = useSelector(state => state.cart.items);
   const dispatch = useDispatch();
 
-  // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
-  };
+    let totalAmount = 0;
 
+    cart.forEach(item => 
+      totalAmount += parseFloat(item.cost.substring(1)) * item.quantity
+    );
+
+    return totalAmount;
+  };
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
-
-
-
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
-
   const handleDecrement = (item) => {
-   
+    const itemToDecrement = cart.find(cartItem => cartItem.name === item.name);
+    if (itemToDecrement.quantity > 1) {
+      dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    } else {
+      dispatch(removeItem(item.name));
+    }
   };
-
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
-
-  // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    let totalCost = 0;
+    totalCost += parseFloat(item.cost.substring(1)) * item.quantity;
+    return totalCost;
+  };
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
   };
 
   return (
@@ -57,12 +67,11 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
+
 };
 
 export default CartItem;
-
-
